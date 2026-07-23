ac4c8b1a4a4b418c7169bd23171c3d5ac25dcf87
Marlon <marlonminango77@gmail.com>
2026-07-22 20:08:31 -0500

    componentes reutilizables

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="system-footer">
+      <p>
+        © 2026 Escuela de Educación Básica República de Venezuela
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
