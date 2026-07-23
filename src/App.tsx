f0fa87fc369ec42a3910ef8d797f062711e5cfb7
Marlon <marlonminango77@gmail.com>
2026-07-22 19:38:45 -0500

    Arreglo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import Asistencia from "./pages/Asistencia/Asistencia";
 
-
-function App() {
-  return <Asistencia />;
-=======
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return <AppRoutes />;
->>>>>>> 249d1d3132565d71bdb435bdac19a63829343306
+
 }
 
 export default App;
